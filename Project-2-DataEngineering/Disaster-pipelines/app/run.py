6b9a93def11ff4580a089585ff3a53417b20053a
Yifang Lin <linyifang.listen@outlook.com>
2021-06-13 22:46:44 +0100

    feat: set up flask web app and add visualisations with Plotly

--- Project-2-DataEngineering/Disaster-pipelines/app/run.py
+++ Project-2-DataEngineering/Disaster-pipelines/app/run.py
@@ -26,11 +26,13 @@ def tokenize(text):
     return clean_tokens
 
 # load data
-engine = create_engine('sqlite:///../data/YourDatabaseName.db')
-df = pd.read_sql_table('YourTableName', engine)
+engine = create_engine('sqlite:///../etl_pipeline.db')
+#engine = create_engine('sqlite:///../data/etl_pipeline.db')
+df = pd.read_sql_table('etl_pipeline', engine)
 
 # load model
-model = joblib.load("../models/your_model_name.pkl")
+model = joblib.load("../classifier.pkl")
+#model = joblib.load("../models/classifier.pkl") #original
 
 
 # index webpage displays cool visuals and receives user input text for model
@@ -42,9 +44,19 @@ def index():
     # TODO: Below is an example - modify to extract data for your own visuals
     genre_counts = df.groupby('genre').count()['message']
     genre_names = list(genre_counts.index)
-
     # create visuals
     # TODO: Below is an example - modify to create your own visuals
+    # for second visualization
+    df_for_graph = df[df.columns[4:]]
+    category_counts=df_for_graph.sum()
+    category_names = df_for_graph.columns.values #list(category_counts.index)
+    import plotly.graph_objects as go
+    genres = ['Direct', 'News', 'Social']
+    # for third visualization
+    related_counts = df[df['related']==1].groupby('genre').count()['related']
+    aid_counts = df[df['aid_related']==1].groupby('genre').count()['aid_related']
+    direct_counts = df[df['direct_report']==1].groupby('genre').count()['direct_report']
+    #horizontal bar graph of category distribution
     graphs = [
         {
             'data': [
@@ -63,9 +75,36 @@ def index():
                     'title': "Genre"
                 }
             }
+        },
+    # second graph
+            {'data': [
+                Bar(
+                    y=category_names,
+                    x=category_counts,
+                    orientation ='h',
+                    marker=dict(color='LightSkyBlue',
+                    line=dict(color='MediumPurple')
+                )
+                )
+            ],
+            'layout': {
+                'title': 'Distribution of categories',
+                'yaxis': {
+                'title': "Categories" #, 'tickangle': 30, 'dtick':1
+            },
+                'xaxis': {
+                    'title': "Count"
+            }
         }
+    },
+    # third graph
+            go.Figure(data=[
+            go.Bar(name='Related', x=genres, y=related_counts),
+            go.Bar(name='Aid Related', x=genres, y=aid_counts),
+            go.Bar(name='Direct Report', x=genres, y=direct_counts)
+        ])
+    
     ]
-
     # encode plotly graphs in JSON
     ids = ["graph-{}".format(i) for i, _ in enumerate(graphs)]
     graphJSON = json.dumps(graphs, cls=plotly.utils.PlotlyJSONEncoder)
